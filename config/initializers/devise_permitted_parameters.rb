8780906379caa494d07f7207f5ce3d9b00552361
Cyrille <c.derche@me.com>
2014-09-04 15:36:55 +0200

    fixed add user for admin issue

--- config/initializers/devise_permitted_parameters.rb
+++ config/initializers/devise_permitted_parameters.rb
@@ -11,8 +11,13 @@ module DevisePermittedParameters
     devise_parameter_sanitizer.for(:sign_up) << :name
     devise_parameter_sanitizer.for(:account_update) << :name
     
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_in) do |u| 
+      u.permit(:email, :password, :remember_me)
+    end
     
+    devise_parameter_sanitizer.for(:invite) do |u|
+      u.permit(:email, :company_id)
+    end
   end
 
 end
