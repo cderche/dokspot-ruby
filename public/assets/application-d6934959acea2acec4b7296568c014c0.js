5c54982fe626419710ca135ed043797ae7d631be
Cyrille Derche <c.derche@me.com>
2014-11-10 18:42:20 +0000

    added favicon.

--- public/assets/application-d6934959acea2acec4b7296568c014c0.js
+++ public/assets/application-d6934959acea2acec4b7296568c014c0.js
@@ -13472,12 +13472,20 @@ return jQuery;
 
 }).call(this);
 (function() {
+
+
+}).call(this);
+(function() {
   $(function() {
     return $(".download").click(function(e) {
       e.preventDefault();
       window.location.href = this.href;
     });
   });
+
+}).call(this);
+(function() {
+
 
 }).call(this);
 (function() {
@@ -13573,3 +13581,11 @@ $(function () {
   $("[data-toggle='tooltip']").tooltip();
   $("[data-toggle='popover']").popover();
 });
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-50717184-1', 'auto');
+  ga('send', 'pageview');
