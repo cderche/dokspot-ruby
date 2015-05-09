0adbe432230dd5851fd39d75902013a0493a80b8
Cyrille Derche <c.derche@me.com>
2015-05-09 16:09:57 +0200

    minor fixes.

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,3 +1,9 @@
 class Language < ActiveRecord::Base
   has_many  :instructions
+
+  validates :name,  presence: true
+  validates :local,	presence: true
+  
+  validates :name,  uniqueness: true
+  validates :local,	uniqueness: true
 end
