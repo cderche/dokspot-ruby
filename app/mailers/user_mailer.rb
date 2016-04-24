a90b973aca69da396f21bbdfb0904697057e5310
Kirill <c.derche@me.com>
2016-04-24 18:44:52 +0200

    migrated mandrill

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,54 +7,73 @@ class UserMailer < Devise::Mailer
 		@mandrill_client ||= Mandrill::API.new MANDRILL_API_KEY
 	end
 
-	def send_email(template_name, record, link)
+	def send_email(template_name, record, args)
 		puts "Sending " + template_name + " to " + record.email
 
 		template_content = []
   	message = {
+  		subaccount: "dokspot",
 			to: [{email: record.email }],
 			merge_vars: [
 				{ 
 					rcpt: record.email,
-					vars: [
-						{
-							name: "LINK", content: link
-						}
-					]
+					vars: args
 				}
 			]
 		}
 
-		mandrill_client.messages.send_template template_name, template_content, message
-		puts "Message has been sent to Mandrill"
+		async = false
+    ip_pool = "Main Pool"
+    begin
+	    result = mandrill_client.messages.send_template template_name, template_content, message, async, ip_pool
+	    puts 'Result ' + result.join(",")
+		#puts "Message has been sent to Mandrill"
+		rescue Mandrill::Error => e
+	    # Mandrill errors are thrown as exceptions
+	    puts "A mandrill error occurred: #{e.class} - #{e.message}"
+	    # A mandrill error occurred: Mandrill::UnknownSubaccountError - No subaccount exists with the id 'customer-123'
+	    raise
+		end
+
 	end
 
 	def confirmation_instructions(record, token, opts={})
-		link = confirmation_url(record, confirmation_token: token)
-    	template_name = "user-confirmation"
-
-    	send_email template_name, record, link
+		args = [{
+			name: "ACTIVATION_LINK",
+			content: confirmation_url(record, confirmation_token: token)
+		}]
+  	template_name = "dokspot-account-invitation"
+  	send_email template_name, record, args
 	end
 
 	def reset_password_instructions(record, token, opts={})
-    	link = edit_password_url(record, reset_password_token: token)
-    	template_name = "user-reset"
+		args = [{
+			name: "RESET_LINK",
+			content: edit_password_url(record, reset_password_token: token)
+		}]
+  	
+  	template_name = "dokspot-account-reset"
+  	send_email template_name, record, args
+  end
 
-    	send_email template_name, record, link
-    end
+  def unlock_instructions(record, token, opts={})
+  	args = [{
+			name: "UNLOCK_LINK",
+			content: unlock_url(record, unlock_token: token)
+		}]
 
-    def unlock_instructions(record, token, opts={})
-    	link = unlock_url(record, unlock_token: token)
-    	template_name = "user-unlock"
+  	template_name = "dokspot-account-unlock"
+  	send_email template_name, record, args
+  end
 
-    	send_email template_name, record, link
-    end
+  def invitation_instructions(record, opts={})
+  	args = [{
+			name: "INVITATION_LINK",
+			content: accept_invitation_url(record, :invitation_token => opts)
+		}]
 
-    def invitation_instructions(record, opts={})
-    	link = accept_invitation_url(record, :invitation_token => opts)
-    	template_name = "user-invitation"
-
-    	send_email template_name, record, link
+  	template_name = "dokspot-account-invitation"
+  	send_email template_name, record, args
 	end
 
 end
--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,13 +1,12 @@
-#MANDRILL_API_KEY = "bSSnAQgslE2rHwsMO1r-nA"
 MANDRILL_API_KEY = ENV['MANDRILL_API_KEY']
 
-#puts "MANDRILL_API_KEY: " + MANDRILL_API_KEY
+puts MANDRILL_API_KEY
 
 ActionMailer::Base.smtp_settings = {
     address: "smtp.mandrillapp.com",
-    port: 2525, #587,
+    port: 587, #2525,
     enable_starttls_auto: true,
-    user_name: "cyrille.derche@dokspot.com",
+    user_name: "c.derche@me.com",
     password: MANDRILL_API_KEY,
     authentication: "login"
 }
